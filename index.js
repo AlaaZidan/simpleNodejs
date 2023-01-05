c6321b31fafefe6c808947e2c8a498f02cd9f8f5
Alaa Zidan <34011501+AlaaZidan@users.noreply.github.com>
2023-01-05 17:34:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
 response.writeHead(200, {'Content-Type': 'text/plain'});
 
 // Send the response body as "Hello World from nodejs"
-response.end('Hello World from nodejs\n');
+response.end('Hello World from nodejs simple alaazidan\n');
 }).listen(8081);
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
